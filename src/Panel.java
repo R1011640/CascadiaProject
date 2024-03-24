417c33103770d82c38145c408784b740802c1ccd
R1011640 <R1011640@students.katyisd.org>
2024-03-24 12:31:03 -0500

    MouseListener

--- src/Panel.java
+++ src/Panel.java
@@ -1,11 +1,32 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
-public class Panel extends JPanel{
+public class Panel extends JPanel implements MouseListener{
+	
+	public Panel() {
+		addMouseListener(this);
+	}
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
 		g.drawLine(0, 0, 100, 100);
 	}
+	
+	
+	public void addNotify() {
+		super.addNotify();
+		requestFocus();
+	}
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("clicked");
+	}
+	
+	
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
 }

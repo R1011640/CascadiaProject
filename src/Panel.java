4f505b9df04de55d488ef2a5b0379e7f9b3ca737
R1011640 <R1011640@T357579-1001L.slhs.katyisd.org>
2024-04-02 11:42:11 -0500

    cc

--- src/Panel.java
+++ src/Panel.java
@@ -17,7 +17,7 @@ public class Panel extends JPanel implements MouseListener{
 	
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
-		Node n = new Node(200, 200);
+		/*Node n = new Node(200, 200);
 		g.drawPolygon(n.hexagon);
 		g.drawLine(200, 200, 
 				200 - (200 - (n.hexagon.xpoints[0] + n.hexagon.xpoints[1])/2)*2, 
@@ -32,6 +32,7 @@ public class Panel extends JPanel implements MouseListener{
 		// 0, pi/3, 2pi/3, 3pi/3, 4pi/3, 5pi/3
 		
 		g.drawPolygon(n2.hexagon);
+		*/
 		
 	}
 	

b1344e8a3522ebd21cb9e40c704ac27e849a4653
R1011640 <R1011640@students.katyisd.org>
2024-04-01 20:04:23 -0500

    Imported Assets

--- src/Node.java
+++ src/Node.java
@@ -6,8 +6,8 @@ public class Node {
 	public Node(int x, int y) {
 		hexagon = new Polygon();
 		for (int i = 0; i < 6; i++){
-			hexagon.addPoint((int) (x + 50 * Math.cos(99 + (i * 2 * Math.PI / 6))),
-					  (int) (y + 50 * Math.sin(99 + (i * 2 * Math.PI / 6))));
+			hexagon.addPoint((int) (x + 50 * Math.cos((i * 2 * Math.PI / 6) + Math.PI/2)),
+					  (int) (y + 50 * Math.sin((i * 2 * Math.PI / 6) + Math.PI/2)));
 		}
 	}
 }
--- src/Panel.java
+++ src/Panel.java
@@ -17,10 +17,20 @@ public class Panel extends JPanel implements MouseListener{
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
 		Node n = new Node(200, 200);
+		g.drawPolygon(n.hexagon);
+		g.drawLine(200, 200, 
+				200 - (200 - (n.hexagon.xpoints[0] + n.hexagon.xpoints[1])/2)*2, 
+				200 - (200 - (n.hexagon.ypoints[0] + n.hexagon.ypoints[1])/2)*2);
 		
-		//g.drawLine(200, 200, 
-		//		200 - (200 - (hexagon.xpoints[0] + hexagon.xpoints[1])/2)*2, 
-		//		200 - (200 - (hexagon.ypoints[0] + hexagon.ypoints[1])/2)*2);
+		
+		
+		Node n2 = new Node((int)(200 - (200 - (n.hexagon.xpoints[0] + n.hexagon.xpoints[1])/2)*2),
+				           (int)(200 - (200 - (n.hexagon.ypoints[0] + n.hexagon.ypoints[1])/2)*2));
+		
+		
+		// 0, pi/3, 2pi/3, 3pi/3, 4pi/3, 5pi/3
+		
+		g.drawPolygon(n2.hexagon);
 		
 	}
 	

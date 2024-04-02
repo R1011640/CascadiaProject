7fba66afcac71fbedb1eee2069242f486b587dda
KolapoCodesSomething <111527597+KolapoCodesSomething@users.noreply.github.com>
2024-04-02 12:13:35 -0500

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,30 @@
 
 public class Game {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private Player [] players;
+	private int currentPlayer = 0;
+	private ArrayList<Node> allNodes;
+	private Node [][] startingNodes;
+	private ArrayList<Character> animals;
+	private String scoringCards;
+	
+	public Game() {
+		
+	}
+	public Node[] getNodes() {
+		
+	}
+	public int [][] scoring(){
+		
+	}
+	public Player currentPlayer() {
+		return currentPlayer;
+	}
+	public void endTurn() {
+		currentPlayer++;
+		if(currentPlayer == 2) {
+			currentPlayer = 0;
+		}
 	}
 
 }

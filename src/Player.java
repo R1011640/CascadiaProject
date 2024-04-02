812a71daf801df5cdcb7dff585683f6bcc8b9923
KolapoCodesSomething <111527597+KolapoCodesSomething@users.noreply.github.com>
2024-04-02 12:14:16 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,22 @@
 
 public class Player {
 	
+	private int natureTokens = 0;
+	private ArrayList<Node> nodes;
+	
+	public Player(Node[] startingNodes) {
+		
+	}
+	public void addNode(int n) {
+		
+	}
+	public ArrayList<Node> getNodes(){
+		
+	}
+	public Node findAnimal(char a) {
+		
+	}
+	public Node findTerrain(char t) {
+		
+	}
 }

d4876ddbf61c0c7c49b1f55423a283b3d2b60d62
Ganapathy Ram School <Ganapathy Ram School@10.67.0.137>
2024-04-05 11:39:17 -0600

    upd

--- src/Node.java
+++ src/Node.java
@@ -72,6 +72,8 @@ public class Node {
 		return "("+x + "," + y + ")";
 	}
 	public int getX() {return x;}
+	public void setX(int x) {this.x = x;}
 	public int getY() {return y;}
+	public void setY(int y) {this.y = y;}
 	public int getSize() {return size;}
 }

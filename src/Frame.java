0e6a1d304e71220bf648d731201e38f524cdf995
Ganapathy Ram School <Ganapathy Ram School@10.67.31.72>
2024-04-22 09:53:01 -0600

    upd

--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,9 @@
 import javax.swing.JFrame;
+import java.awt.Toolkit;
 
 public class Frame extends JFrame{
 	private static final int WIDTH = 1920;
 	private static final int HEIGHT = 1080;
-	
 	public Frame (String framename) {
 		super(framename);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
